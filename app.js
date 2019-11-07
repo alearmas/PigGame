652713b0c56f4bf79cba07466692363f589b6b77
Armas Eduardo <a.armas.eduardo@accenture.com>
2019-11-07 17:20:28 -0300

    aplicando querySelectors

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
+var scores, roundScores, activePlayer, dice;
+
+scores = [0,0];
+roundScores = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-0').textContent;
+
+document.querySelector('.dice').style.display = 'none';
