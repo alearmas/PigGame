25fadd936c07c646c40a992b1d9ac3930d7f1ac8
Armas Eduardo <a.armas.eduardo@accenture.com>
2019-11-10 18:20:25 -0300

    Terminando función de inicio

--- app.js
+++ app.js
@@ -7,18 +7,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     // 1. Random number 
@@ -37,8 +28,6 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     } else {
         nextPlayer();
     }
-
-
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
@@ -57,11 +46,30 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     } else {
         //Check if player won the game
         nextPlayer();
-
     }
-    
-
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = "Player 1";
+    document.getElementById('name-1').textContent = "Player 2";
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+};
 
 function nextPlayer(){
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -75,5 +83,3 @@ function nextPlayer(){
 
     document.querySelector('.dice').style.display = 'none';
 }
-
-//var x = document.querySelector('#score-0').textContent;
