da111f1c46b8b95d33ec464411eec642e4f61ef8
Armas Eduardo <a.armas.eduardo@accenture.com>
2019-11-10 12:01:48 -0300

    usando el boton hold y ganador

--- app.js
+++ app.js
@@ -35,20 +35,45 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    //Update the UI(User Interface)
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //Check if player won the game
+    if(scores[activePlayer] >= 100){
+        document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        //Check if player won the game
+        nextPlayer();
+
+    }
+    
+
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 //var x = document.querySelector('#score-0').textContent;
