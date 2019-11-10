15f96c8c426ec3b3b9aa1c825be549d5da1bce01
Armas Eduardo <a.armas.eduardo@accenture.com>
2019-11-10 11:22:24 -0300

    cambiando de jugador

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ GAME RULES:
 var scores, roundScores, activePlayer, dice;
 
 scores = [0,0];
-roundScores = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -27,13 +27,28 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.src = 'img/dice-' + dice + '.png';
     
-    // 3. Update 
+    // 3. Update the round score IF the rolled numberwas NOT a 1
+    if(dice !== 1) {
+        //Add score 
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+
 });
 
-
-/* document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-0').textContent;
- */
+//var x = document.querySelector('#score-0').textContent;
