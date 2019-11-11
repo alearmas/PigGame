9b06900ae95ccb5f6c69ecfea75e69bfd0653636
Armas Eduardo <a.armas.eduardo@accenture.com>
2019-11-11 09:54:40 -0300

    bugs resueltos

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         var dice2 = Math.floor(Math.random() * 6) + 1;
         
         // 2. Display the result
-        document.getElementById('dice-1').style.display = 'block';
-        document.getElementById('dice-2').style.display = 'block';
-        document.getElementById('dice-1').src = 'img/dice-' + dice1 + '.png';
-        document.getElementById('dice-2').src = 'img/dice-' + dice2 + '.png';
+        document.getElementById('dice-01').style.display = 'block';
+        document.getElementById('dice-02').style.display = 'block';
+        document.getElementById('dice-01').src = 'img/dice-' + dice1 + '.png';
+        document.getElementById('dice-02').src = 'img/dice-' + dice2 + '.png';
         
         // 3. Update the round score IF the rolled numberwas NOT a 1
         if(dice1 !== 1 && dice2 !== 1) {
@@ -70,8 +70,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         //Check if player won the game
         if(scores[activePlayer] >= winningScore){
             document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
-            document.getElementById('dice-1').style.display = 'none';
-            document.getElementById('dice-2').style.display = 'none';
+            document.getElementById('dice-01').style.display = 'none';
+            document.getElementById('dice-02').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -90,8 +90,8 @@ function init(){
     roundScore = 0;
     gamePlaying = true;
 
-    document.getElementById('dice-1').style.display = 'none';
-    document.getElementById('dice-2').style.display = 'none';
+    document.getElementById('dice-01').style.display = 'none';
+    document.getElementById('dice-02').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -116,6 +116,6 @@ function nextPlayer(){
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.getElementById('dice-1').style.display = 'none';
-    document.getElementById('dice-2').style.display = 'none';
+    document.getElementById('dice-01').style.display = 'none';
+    document.getElementById('dice-02').style.display = 'none';
 }
